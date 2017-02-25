0ecdcf74dde63fc882aff3580e4773f3009cdbe4
Cristian Muscalu <muscaiu@yahoo.com>
2017-02-25 15:29:02 +0200

    loading jan and feb into chart

--- public/app/controllers/chartCtrl.js
+++ public/app/controllers/chartCtrl.js
@@ -6,35 +6,44 @@ angular.module("chartControllers", ["chart.js", 'interviewServices'])
 
     $scope.labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     $scope.series = ['Interviews', 'Employees'];
+    $scope.data = []
 
-    $scope.data = [
-        [65, 59, 80, 81, 56, 55, 40, 80, 81, 56, 55, 40],
-        [28, 48, 40, 19, 86, 27, 90, 80, 81, 56, 55, 40]
-    ];
+    var jan2017Total = 0
+    var feb2017Total = 0
 
     this.loadChartData = function() {
         Interview.getChartData().then(function(response) {
-            var jan2017Total = 0
+
+            $scope.data = []
 
             response.data.forEach(function(element) {
                 var momenYear = moment(element.dataapplicazione).format('YYYY')
 
                 if (momenYear == 2017) {
                     var momentDate = moment(element.dataapplicazione).format('M')
-
                     if (momentDate == 1) {
                         jan2017Total++
+                    } else
+                    if (momentDate == 2) {
+                        feb2017Total++
                     }
-                    console.log("2017:", momentDate)
-
                 } else if (momenYear == 2016) {
-                    var momentDate = moment(element.dataapplicazione).format('M')
-                    console.log("2016:", momentDate)
+
                 } else {
                     // console.log(momenYear)
                 }
             }, this);
             console.log('jan2017Total', jan2017Total)
+            console.log('feb2017Total', feb2017Total)
+
+            $scope.data.push(jan2017Total)
+            $scope.data.push(feb2017Total)
+
+            console.log($scope.data)
         })
     }
+
+    app.loadChartData()
+
+
 });
