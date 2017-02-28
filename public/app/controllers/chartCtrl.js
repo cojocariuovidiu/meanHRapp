ac03c3f825e9b68794f2e5f9c83c8997ffe48355
Cristian Muscalu <muscaiu@yahoo.com>
2017-02-28 10:39:35 +0200

    filtering by 2016 and 2017 with same code, still need more DRY

--- public/app/controllers/chartCtrl.js
+++ public/app/controllers/chartCtrl.js
@@ -1,16 +1,6 @@
-angular.module("chartControllers", ["chart.js", 'interviewServices'])
+'use strict'
 
-// .config(['ChartJsProvider', function(ChartJsProvider) {
-//     // Configure all charts
-//     ChartJsProvider.setOptions({
-//         //chartColors: ['#FF5252', '#FF8A80'],
-//         responsive: false
-//     });
-//     // Configure all line charts
-//     ChartJsProvider.setOptions('line', {
-//         showLines: false
-//     });
-// }])
+angular.module("chartControllers", ["chart.js", 'interviewServices'])
 
 .controller("chartCtrl", function(Interview, $scope) {
 
@@ -27,121 +17,121 @@ angular.module("chartControllers", ["chart.js", 'interviewServices'])
     app.barChart.Interviews = []
     app.barChart.Employees = []
 
-    // $scope.options = { legend: { display: true } }; // missing 
-    // $scope.labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    // $scope.series = ['Interviews', 'Employees'];
-    //$scope.data = []
-
     this.loadChartData = function(option) {
         $scope.selectedYear = option
         app.barChart.data = []
         app.barChart.Interviews = []
         app.barChart.Employees = []
-            // app.barChart.Employees = []
 
         if (option == 2017) {
-            Interview.getChartData().then(function(response) {
-                janTotal = 0
-                febTotal = 0
-                marTotal = 0
-                aprTotal = 0
-                mayTotal = 0
-                junTotal = 0
-                julTotal = 0
-                augTotal = 0
-                sepTotal = 0
-                oct2017Total = 0
-                nov2017Total = 0
-                dec2017Total = 0
-                jan2017Emp = 0
-                feb2017Emp = 0
-                mar2017Emp = 0
-                apr2017Emp = 0
-                mai2017Emp = 0
-                jun2017Emp = 0
-                jul2017Emp = 0
-                aug2017Emp = 0
-                sep2017Emp = 0
-                oct2017Emp = 0
-                nov2017Emp = 0
-                dec2017Emp = 0
+            ChartFilterYear()
+        } else if (option == 2016) {
+            ChartFilterYear()
+        }
 
+        function ChartFilterYear() {
+            var janTotal = 0
+            var febTotal = 0
+            var marTotal = 0
+            var aprTotal = 0
+            var mayTotal = 0
+            var junTotal = 0
+            var julTotal = 0
+            var augTotal = 0
+            var sepTotal = 0
+            var octTotal = 0
+            var novTotal = 0
+            var decTotal = 0
+            var janEmp = 0
+            var febEmp = 0
+            var marEmp = 0
+            var aprEmp = 0
+            var mayEmp = 0
+            var junEmp = 0
+            var julEmp = 0
+            var augEmp = 0
+            var sepEmp = 0
+            var octEmp = 0
+            var novEmp = 0
+            var decEmp = 0
+
+            Interview.getChartData().then(function(response) {
                 response.data.forEach(function(element) {
                     var interviewStatus = element.interviewStatus
-
                     var momenYear = moment(element.dataapplicazione).format('YYYY')
-                    if (momenYear == 2017) {
+
+                    if (momenYear == option) {
                         var momentDate = moment(element.dataapplicazione).format('M')
                         if (momentDate == 1) {
                             janTotal++
                             if (element.interviewStatus == 'isEmployee') {
-                                jan2017Emp++
+                                janEmp++
                             }
                         } else
                         if (momentDate == 2) {
                             febTotal++
                             if (element.interviewStatus == 'isEmployee') {
-                                feb2017Emp++
+                                febEmp++
                             }
                         } else
                         if (momentDate == 3) {
                             marTotal++
                             if (element.interviewStatus == 'isEmployee') {
-                                mar2017Emp++
+                                marEmp++
                             }
                         } else
                         if (momentDate == 4) {
                             aprTotal++
                             if (element.interviewStatus == 'isEmployee') {
-                                apr2017Emp++
+                                aprEmp++
                             }
                         } else
                         if (momentDate == 5) {
                             mayTotal++
                             if (element.interviewStatus == 'isEmployee') {
-                                mai2017Emp++
+                                mayEmp++
                             }
                         } else
                         if (momentDate == 6) {
                             junTotal++
                             if (element.interviewStatus == 'isEmployee') {
-                                jun2017Emp++
+                                junEmp++
                             }
                         } else
                         if (momentDate == 7) {
                             julTotal++
                             if (element.interviewStatus == 'isEmployee') {
-                                jul2017Emp++
+                                julEmp++
                             }
                         } else
                         if (momentDate == 8) {
                             augTotal++
                             if (element.interviewStatus == 'isEmployee') {
-                                aug2017Emp++
+                                augEmp++
                             }
                         } else
                         if (momentDate == 9) {
                             sepTotal++
                             if (element.interviewStatus == 'isEmployee') {
-                                sep2017Emp++
+                                sepEmp++
                             }
                         } else
                         if (momentDate == 10) {
-                            oct2017Total++
+                            octTotal++
                             if (element.interviewStatus == 'isEmployee') {
-                                oct2017Emp++
+                                octEmp++
                             }
                         } else
                         if (momentDate == 11) {
-                            nov2017Total++
+                            novTotal++
                             if (element.interviewStatus == 'isEmployee') {
-                                nov2017Emp++
+                                novEmp++
                             }
                         } else
                         if (momentDate == 12) {
-                            dec2017Total++
+                            decTotal++
                             if (element.interviewStatus == 'isEmployee') {
-                                dec2017Emp++
+                                decEmp++
                             }
                         }
                     } else {
@@ -149,85 +139,15 @@ angular.module("chartControllers", ["chart.js", 'interviewServices'])
                     }
                 }, this);
 
-                app.barChart.Interviews.push(janTotal, febTotal, marTotal, aprTotal, mayTotal, junTotal, julTotal, augTotal, sepTotal, oct2017Total, nov2017Total, dec2017Total)
-                app.barChart.Employees.push(jan2017Emp, feb2017Emp, mar2017Emp, apr2017Emp, mai2017Emp, jun2017Emp, jul2017Emp, aug2017Emp, sep2017Emp, oct2017Emp, nov2017Emp, dec2017Emp)
+                app.barChart.Interviews.push(janTotal, febTotal, marTotal, aprTotal, mayTotal, junTotal, julTotal, augTotal, sepTotal, octTotal, novTotal, decTotal)
+                app.barChart.Employees.push(janEmp, febEmp, marEmp, aprEmp, mayEmp, junEmp, julEmp, augEmp, sepEmp, octEmp, novEmp, decEmp)
 
                 app.barChart.data.push(app.barChart.Interviews)
                 app.barChart.data.push(app.barChart.Employees)
 
-                console.log(app.barChart.data)
-            })
-        } else if (option == 2016) {
-
-            Interview.getChartData().then(function(response) {
-                jan2016Total = 0
-                feb2016Total = 0
-                mar2016Total = 0
-                apr2016Total = 0
-                mai2016Total = 0
-                jun2016Total = 0
-                jul2016Total = 0
-                aug2016Total = 0
-                sep2016Total = 0
-                oct2016Total = 0
-                nov2016Total = 0
-                dec2016Total = 0
-
-                response.data.forEach(function(element) {
-                    var momenYear = moment(element.dataapplicazione).format('YYYY')
-                    if (momenYear == 2016) {
-                        var momentDate = moment(element.dataapplicazione).format('M')
-                        if (momentDate == 1) {
-                            jan2016Total++
-                        } else if (momentDate == 2) {
-                            feb2016Total++
-                        } else if (momentDate == 3) {
-                            mar2016Total++
-                        } else if (momentDate == 4) {
-                            apr2016Total++
-                        } else if (momentDate == 5) {
-                            mai2016Total++
-                        } else if (momentDate == 6) {
-                            jun2016Total++
-                        } else if (momentDate == 7) {
-                            jul2016Total++
-                        } else if (momentDate == 8) {
-                            aug2016Total++
-                        } else if (momentDate == 9) {
-                            sep2016Total++
-                        } else if (momentDate == 10) {
-                            oct2016Total++
-                        } else if (momentDate == 11) {
-                            nov2016Total++
-                        } else if (momentDate == 12) {
-                            dec2016Total++
-                        }
-                    } else {
-                        //console.log(momenYear)
-                    }
-                }, this);
-
-                app.barChart.Interviews.push(jan2016Total)
-                app.barChart.Interviews.push(feb2016Total)
-                app.barChart.Interviews.push(mar2016Total)
-                app.barChart.Interviews.push(apr2016Total)
-                app.barChart.Interviews.push(mai2016Total)
-                app.barChart.Interviews.push(jun2016Total)
-                app.barChart.Interviews.push(jul2016Total)
-                app.barChart.Interviews.push(aug2016Total)
-                app.barChart.Interviews.push(jun2016Total)
-                app.barChart.Interviews.push(jul2016Total)
-                app.barChart.Interviews.push(aug2016Total)
-                app.barChart.Interviews.push(sep2016Total)
-                app.barChart.Interviews.push(oct2016Total)
-                app.barChart.Interviews.push(nov2016Total)
-                app.barChart.Interviews.push(dec2016Total)
-                app.barChart.data.push(app.barChart.Interviews)
-                app.barChart.data.push(app.barChart.Employees)
                 console.log(app.barChart.data)
             })
         }
-
     }
 
     $scope.$on('chart-destroy', function(evt, chart) {
@@ -248,3 +168,15 @@ angular.module("chartControllers", ["chart.js", 'interviewServices'])
     app.loadChartData(2017)
 
 });
+
+// .config(['ChartJsProvider', function(ChartJsProvider) {
+//     // Configure all charts
+//     ChartJsProvider.setOptions({
+//         //chartColors: ['#FF5252', '#FF8A80'],
+//         responsive: false
+//     });
+//     // Configure all line charts
+//     ChartJsProvider.setOptions('line', {
+//         showLines: false
+//     });
+// }])
