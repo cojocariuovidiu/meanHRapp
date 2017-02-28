cbeda2afeb37a475f4b5c950fe9af69f629f0aec
Cristian Muscalu <muscaiu@yahoo.com>
2017-02-28 10:57:18 +0200

    reformated and completed chart controller

--- public/app/controllers/chartCtrl.js
+++ public/app/controllers/chartCtrl.js
@@ -17,6 +17,7 @@ angular.module("chartControllers", ["chart.js", 'interviewServices'])
     app.barChart.Interviews = []
     app.barChart.Employees = []
 
+    //Button/OnLoad function
     this.loadChartData = function(option) {
         $scope.selectedYear = option
         app.barChart.data = []
@@ -24,130 +25,136 @@ angular.module("chartControllers", ["chart.js", 'interviewServices'])
         app.barChart.Employees = []
 
         if (option == 2017) {
-            ChartFilterYear()
+            ChartFilterYear(option)
         } else if (option == 2016) {
-            ChartFilterYear()
+            ChartFilterYear(option)
+        } else {
+            console.log('chart year != 2016 or 2017')
         }
+    }
 
-        function ChartFilterYear() {
-            var janTotal = 0
-            var febTotal = 0
-            var marTotal = 0
-            var aprTotal = 0
-            var mayTotal = 0
-            var junTotal = 0
-            var julTotal = 0
-            var augTotal = 0
-            var sepTotal = 0
-            var octTotal = 0
-            var novTotal = 0
-            var decTotal = 0
-            var janEmp = 0
-            var febEmp = 0
-            var marEmp = 0
-            var aprEmp = 0
-            var mayEmp = 0
-            var junEmp = 0
-            var julEmp = 0
-            var augEmp = 0
-            var sepEmp = 0
-            var octEmp = 0
-            var novEmp = 0
-            var decEmp = 0
+    //Load 2017 on start
+    app.loadChartData(2017)
 
-            Interview.getChartData().then(function(response) {
-                response.data.forEach(function(element) {
-                    var interviewStatus = element.interviewStatus
-                    var momenYear = moment(element.dataapplicazione).format('YYYY')
+    //Filter function used for each year
+    function ChartFilterYear(option) {
+        var janTotal = 0
+        var febTotal = 0
+        var marTotal = 0
+        var aprTotal = 0
+        var mayTotal = 0
+        var junTotal = 0
+        var julTotal = 0
+        var augTotal = 0
+        var sepTotal = 0
+        var octTotal = 0
+        var novTotal = 0
+        var decTotal = 0
+        var janEmp = 0
+        var febEmp = 0
+        var marEmp = 0
+        var aprEmp = 0
+        var mayEmp = 0
+        var junEmp = 0
+        var julEmp = 0
+        var augEmp = 0
+        var sepEmp = 0
+        var octEmp = 0
+        var novEmp = 0
+        var decEmp = 0
 
-                    if (momenYear == option) {
-                        var momentDate = moment(element.dataapplicazione).format('M')
-                        if (momentDate == 1) {
-                            janTotal++
-                            if (element.interviewStatus == 'isEmployee') {
-                                janEmp++
-                            }
-                        } else
-                        if (momentDate == 2) {
-                            febTotal++
-                            if (element.interviewStatus == 'isEmployee') {
-                                febEmp++
-                            }
-                        } else
-                        if (momentDate == 3) {
-                            marTotal++
-                            if (element.interviewStatus == 'isEmployee') {
-                                marEmp++
-                            }
-                        } else
-                        if (momentDate == 4) {
-                            aprTotal++
-                            if (element.interviewStatus == 'isEmployee') {
-                                aprEmp++
-                            }
-                        } else
-                        if (momentDate == 5) {
-                            mayTotal++
-                            if (element.interviewStatus == 'isEmployee') {
-                                mayEmp++
-                            }
-                        } else
-                        if (momentDate == 6) {
-                            junTotal++
-                            if (element.interviewStatus == 'isEmployee') {
-                                junEmp++
-                            }
-                        } else
-                        if (momentDate == 7) {
-                            julTotal++
-                            if (element.interviewStatus == 'isEmployee') {
-                                julEmp++
-                            }
-                        } else
-                        if (momentDate == 8) {
-                            augTotal++
-                            if (element.interviewStatus == 'isEmployee') {
-                                augEmp++
-                            }
-                        } else
-                        if (momentDate == 9) {
-                            sepTotal++
-                            if (element.interviewStatus == 'isEmployee') {
-                                sepEmp++
-                            }
-                        } else
-                        if (momentDate == 10) {
-                            octTotal++
-                            if (element.interviewStatus == 'isEmployee') {
-                                octEmp++
-                            }
-                        } else
-                        if (momentDate == 11) {
-                            novTotal++
-                            if (element.interviewStatus == 'isEmployee') {
-                                novEmp++
-                            }
-                        } else
-                        if (momentDate == 12) {
-                            decTotal++
-                            if (element.interviewStatus == 'isEmployee') {
-                                decEmp++
-                            }
+        Interview.getChartData().then(function(response) {
+            response.data.forEach(function(element) {
+                var interviewStatus = element.interviewStatus
+                var momenYear = moment(element.dataapplicazione).format('YYYY')
+
+                if (momenYear == option) {
+                    var momentDate = moment(element.dataapplicazione).format('M')
+                    if (momentDate == 1) {
+                        janTotal++
+                        if (element.interviewStatus == 'isEmployee') {
+                            janEmp++
                         }
-                    } else {
-                        //console.log(momenYear)
+                    } else
+                    if (momentDate == 2) {
+                        febTotal++
+                        if (element.interviewStatus == 'isEmployee') {
+                            febEmp++
+                        }
+                    } else
+                    if (momentDate == 3) {
+                        marTotal++
+                        if (element.interviewStatus == 'isEmployee') {
+                            marEmp++
+                        }
+                    } else
+                    if (momentDate == 4) {
+                        aprTotal++
+                        if (element.interviewStatus == 'isEmployee') {
+                            aprEmp++
+                        }
+                    } else
+                    if (momentDate == 5) {
+                        mayTotal++
+                        if (element.interviewStatus == 'isEmployee') {
+                            mayEmp++
+                        }
+                    } else
+                    if (momentDate == 6) {
+                        junTotal++
+                        if (element.interviewStatus == 'isEmployee') {
+                            junEmp++
+                        }
+                    } else
+                    if (momentDate == 7) {
+                        julTotal++
+                        if (element.interviewStatus == 'isEmployee') {
+                            julEmp++
+                        }
+                    } else
+                    if (momentDate == 8) {
+                        augTotal++
+                        if (element.interviewStatus == 'isEmployee') {
+                            augEmp++
+                        }
+                    } else
+                    if (momentDate == 9) {
+                        sepTotal++
+                        if (element.interviewStatus == 'isEmployee') {
+                            sepEmp++
+                        }
+                    } else
+                    if (momentDate == 10) {
+                        octTotal++
+                        if (element.interviewStatus == 'isEmployee') {
+                            octEmp++
+                        }
+                    } else
+                    if (momentDate == 11) {
+                        novTotal++
+                        if (element.interviewStatus == 'isEmployee') {
+                            novEmp++
+                        }
+                    } else
+                    if (momentDate == 12) {
+                        decTotal++
+                        if (element.interviewStatus == 'isEmployee') {
+                            decEmp++
+                        }
                     }
-                }, this);
+                } else {
+                    //console.log(momenYear)
+                }
+            }, this);
 
-                app.barChart.Interviews.push(janTotal, febTotal, marTotal, aprTotal, mayTotal, junTotal, julTotal, augTotal, sepTotal, octTotal, novTotal, decTotal)
-                app.barChart.Employees.push(janEmp, febEmp, marEmp, aprEmp, mayEmp, junEmp, julEmp, augEmp, sepEmp, octEmp, novEmp, decEmp)
+            app.barChart.Interviews.push(janTotal, febTotal, marTotal, aprTotal, mayTotal, junTotal, julTotal, augTotal, sepTotal, octTotal, novTotal, decTotal)
+            app.barChart.Employees.push(janEmp, febEmp, marEmp, aprEmp, mayEmp, junEmp, julEmp, augEmp, sepEmp, octEmp, novEmp, decEmp)
 
-                app.barChart.data.push(app.barChart.Interviews)
-                app.barChart.data.push(app.barChart.Employees)
+            app.barChart.data.push(app.barChart.Interviews)
+            app.barChart.data.push(app.barChart.Employees)
 
-                console.log(app.barChart.data)
-            })
-        }
+            console.log(app.barChart.data)
+        })
     }
 
     $scope.$on('chart-destroy', function(evt, chart) {
@@ -156,16 +163,6 @@ angular.module("chartControllers", ["chart.js", 'interviewServices'])
     $scope.$on('chart-update', function(evt, chart) {
         console.log('update');
     });
-
-    // $scope.$on('chart-create', function(evt, chart) {
-    //     console.log(chart);
-    // });
-
-    // $scope.onClick = function(points, evt) {
-    //     console.log(points, evt);
-    // };
-
-    app.loadChartData(2017)
 
 });
 
@@ -180,3 +177,13 @@ angular.module("chartControllers", ["chart.js", 'interviewServices'])
 //         showLines: false
 //     });
 // }])
+
+
+
+// $scope.$on('chart-create', function(evt, chart) {
+//     console.log(chart);
+// });
+
+// $scope.onClick = function(points, evt) {
+//     console.log(points, evt);
+// };
