86d522d03b880ac91f2bc72423a3776139049322
Cristian Muscalu <muscaiu@yahoo.com>
2017-03-07 18:03:03 +0200

    add test cards

--- public/app/controllers/empCtrl.js
+++ public/app/controllers/empCtrl.js
@@ -2,6 +2,13 @@
 
 angular.module("employeeControllers", [])
 
+.config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
+    $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
+    $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
+    $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
+})
+
 .controller("empCtrl", function($timeout, uploadFile, shareData, Employee, $mdDialog, $scope, $http) {
     var emp = this
     var displayingObject = {}
