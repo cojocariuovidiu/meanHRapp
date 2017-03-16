a1d47135c83247d23a5f399a8f98e2505fb1b665
Desktop <muscaiu@yahoo.com>
2017-03-16 13:27:49 +0200

    update to last commit

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -71,7 +71,7 @@ angular.module('mainController', ['ngMaterial'])
                     //loading twich to hack the error :-(
                     // getInterviewsFiltered('All')
                     console.log('got int on login')
-                    $location.path('/interviews')
+                    $location.path('/interviste')
                     main.isLoading = false
                     main.loginData = null;
                     main.successMsg = false;
